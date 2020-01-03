3228af6215365bda80b546496dd83efaae2be60f
Alessia Bellisario <alessia@breather.com>
2020-01-03 11:21:45 -0500

    Working, just need to figure out descenders

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod utils;
-// use footile::{FillRule, Plotter, Raster, Rgba8};
+use glyph_brush_layout::{
+    rusttype::{point, Font, Scale},
+    *,
+};
 use image::{DynamicImage, Rgba};
-use rusttype::{point, Font, Scale};
 use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
@@ -35,85 +37,78 @@ pub fn generate_text(
     let rgb_value: (u8, u8, u8) = serde_wasm_bindgen::from_value(rgb).unwrap();
 
     // Load the font
-    let font_data = include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf");
+    // let font_data = include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf");
     // This only succeeds if collection consists of one font
-    let font = Font::from_bytes(font_data as &[u8]).expect("Error constructing Font");
+    let font = Font::from_bytes(&include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf")[..])
+        .expect("Error constructing Font");
+    // // Load the font
+    // let font_data = include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf");
+    // // This only succeeds if collection consists of one font
+    // let font = Font::from_bytes(font_data as &[u8]).expect("Error constructing Font");
 
-    // Title font size to use
-    let title_scale = Scale::uniform(title_font_size as f32);
-    let author_scale = Scale::uniform(subtitle_font_size as f32);
+    // let dejavu =
+    //     Font::from_bytes(&include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf")[..]).unwrap();
+    // let garamond =
+    //     Font::from_bytes(&include_bytes!("../fonts/GT-Pressura-Mono-Bold.ttf")[..]).unwrap();
+    let fonts = vec![font];
 
-    let v_title_metrics = font.v_metrics(title_scale);
-    let v_author_metrics = font.v_metrics(author_scale);
+    let title_glyphs: Vec<_> = Layout::default().calculate_glyphs(
+        &fonts,
+        &SectionGeometry {
+            screen_position: (0.0, 0.0),
+            bounds: (WIDTH, HEIGHT),
+        },
+        &[SectionText {
+            text: title,
+            scale: Scale::uniform(title_font_size as f32),
+            font_id: FontId(0),
+            color: [0.0, 1.0, 0.0, 1.0],
+        }],
+    );
 
-    // layout the glyphs in a line with 10 pixels padding
-    let title_glyphs: Vec<_> = font
-        .layout(title, title_scale, point(0.0, 0.0 + v_title_metrics.ascent))
-        .collect();
-    let author_glyphs: Vec<_> = font
-        .layout(
-            author,
-            author_scale,
-            point(0.0, 0.0 + v_author_metrics.ascent),
-        )
-        .collect();
-
-    // work out the layout size
-    // let glyphs_height = (v_title_metrics.ascent - v_title_metrics.descent).ceil() as u32;
-    // let glyphs_width = {
-    //     let min_x = title_glyphs
-    //         .first()
-    //         .map(|g| g.pixel_bounding_box().unwrap().min.x)
-    //         .unwrap();
-    //     let max_x = title_glyphs
-    //         .last()
-    //         .map(|g| g.pixel_bounding_box().unwrap().max.x)
-    //         .unwrap();
-    //     (max_x - min_x) as u32
-    // };
-
-    // log!("height: {}", glyphs_height);
-    // log!("width: {}", glyphs_width);
+    let author_glyphs: Vec<_> = Layout::default().calculate_glyphs(
+        &fonts,
+        &SectionGeometry {
+            screen_position: (0.0, 0.0),
+            bounds: (WIDTH, HEIGHT),
+        },
+        &[SectionText {
+            text: author,
+            scale: Scale::uniform(subtitle_font_size as f32),
+            font_id: FontId(0),
+            color: [0.0, 1.0, 0.0, 1.0],
+        }],
+    );
 
     // Create a new rgba image with some padding
     let mut image = DynamicImage::new_rgba8(WIDTH as u32, HEIGHT as u32).to_rgba();
 
-    // Loop through the glyphs in the text, positing each one on a line
     for glyph in title_glyphs {
-        if let Some(bounding_box) = glyph.pixel_bounding_box() {
+        if let Some(bounding_box) = glyph.0.pixel_bounding_box() {
             // Draw the glyph into the image per-pixel by using the draw closure
-            log!("{:?}", glyph);
-            glyph.draw(|x, y, v| {
-                // log!("position x: {}", glyph.position().x);
-                if x + bounding_box.min.x as u32 + PADDING as u32 >= WIDTH as u32 {
-                    image.put_pixel(
-                        // Offset the position by the glyph bounding box
-                        x + bounding_box.min.x as u32 + PADDING as u32 - WIDTH as u32,
-                        y + bounding_box.min.y as u32 + glyph.position().y as u32 + PADDING as u32,
-                        // Turn the coverage into an alpha value
-                        Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
-                    )
-                } else {
-                    image.put_pixel(
-                        // Offset the position by the glyph bounding box
-                        x + bounding_box.min.x as u32 + PADDING as u32,
-                        y + bounding_box.min.y as u32 + PADDING as u32,
-                        // Turn the coverage into an alpha value
-                        Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
-                    )
-                }
+            // log!("{:?}", glyph);
+            glyph.0.draw(|x, y, v| {
+                image.put_pixel(
+                    // Offset the position by the glyph bounding box
+                    x + bounding_box.min.x as u32,
+                    y + bounding_box.min.y as u32,
+                    // Turn the coverage into an alpha value
+                    Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
+                )
             });
         }
     }
 
     for glyph in author_glyphs {
-        if let Some(bounding_box) = glyph.pixel_bounding_box() {
+        if let Some(bounding_box) = glyph.0.pixel_bounding_box() {
             // Draw the glyph into the image per-pixel by using the draw closure
-            glyph.draw(|x, y, v| {
+            glyph.0.draw(|x, y, v| {
                 image.put_pixel(
                     // Offset the position by the glyph bounding box
-                    x + bounding_box.min.x as u32 + PADDING as u32,
-                    y + bounding_box.min.y as u32 + HEIGHT as u32 - PADDING as u32 - 60,
+                    x + bounding_box.min.x as u32,
+                    (HEIGHT as u32 - bounding_box.max.y as u32) + (y + bounding_box.min.y as u32)
+                        - PADDING as u32,
+                    // y + bounding_box.min.y as u32 + HEIGHT as u32 - PADDING as u32 - 60,
                     // Turn the coverage into an alpha value
                     Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
                 )
@@ -122,52 +117,4 @@ pub fn generate_text(
     }
 
     return image.to_vec();
-
-    //////
-    ////
-    //
-    ////
-    /////
-
-    // // Init font, and paths
-    // let font = if has_serif_font {
-    //     fonterator::normal_font()
-    // } else {
-    //     fonterator::monospace_font()
-    // };
-
-    // // Init rendering
-    // let mut plotter = Plotter::new(WIDTH as u32, HEIGHT as u32);
-    // let mut raster = Raster::new(plotter.width(), plotter.height());
-
-    // // Render title left aligned with line wrapping
-    // let mut begin = 0;
-    // let mut line = 0;
-    // loop {
-    //     let (path, l) = font.render(
-    //         &title[begin..],
-    //         (
-    //             PADDING,
-    //             line as f32 * title_font_size as f32 + PADDING,
-    //             WIDTH - (PADDING * 2.0),
-    //             HEIGHT,
-    //         ),
-    //         (title_font_size as f32, title_font_size as f32),
-    //         fonterator::TextAlign::Left,
-    //     );
-    //     let path: Vec<footile::PathOp> = path.collect();
-    //     raster.over(
-    //         plotter.fill(&path, FillRule::NonZero),
-    //         Rgba8::rgb(rgb_value.0, rgb_value.1, rgb_value.2),
-    //     );
-    //     begin += l;
-    //     line += 1;
-    //     if l == 0 {
-    //         break;
-    //     }
-    // }
-
-    // // Return a Vec<T>
-    // let x = raster.as_u8_slice().to_vec();
-    // return x;
 }
