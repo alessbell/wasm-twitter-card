75185bc14d958be55559622c9297bdf8fe6a1146
Alessia Bellisario <github@bellisar.io>
2020-01-03 16:50:25 -0500

    Use rusttype instead of fonterator (#2)
    * WIP use new lib
    
    * Finicky line breaks the hard way
    
    * Working, just need to figure out descenders
    
    * Reads font binary into Uint8Array

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 mod utils;
-use footile::{FillRule, Plotter, Raster, Rgba8};
+use glyph_brush_layout::{
+    rusttype::{Font, Scale},
+    *,
+};
+use image::{DynamicImage, Rgba};
+use js_sys::Uint8Array;
 use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
@@ -23,74 +28,87 @@ pub fn generate_text(
     title_font_size: i32,
     subtitle_font_size: i32,
     rgb: JsValue,
-    has_serif_font: bool,
+    font_style: &str,
+    font_file: Uint8Array,
 ) -> Vec<u8> {
     utils::set_panic_hook();
-    let rgb_value: (u8, u8, u8) = serde_wasm_bindgen::from_value(rgb).unwrap();
-
     const WIDTH: f32 = 1200.0;
     const HEIGHT: f32 = 630.0;
     const PADDING: f32 = 50.0;
 
-    // Init font, and paths
-    let font = if has_serif_font {
-        fonterator::normal_font()
+    let rgb_value: (u8, u8, u8) = serde_wasm_bindgen::from_value(rgb).unwrap();
+
+    let font = if font_style == "monospace" {
+        Font::from_bytes(&include_bytes!("../fonts/Inconsolata-Medium.ttf")[..])
+            .expect("Error constructing Font")
+    } else if font_style == "sans-serif" {
+        Font::from_bytes(&include_bytes!("../fonts/OpenSans-Regular.ttf")[..])
+            .expect("Error constructing Font")
     } else {
-        fonterator::monospace_font()
+        // take font binary passed in as Uint8Array and copy the contents into a new vec
+        let user_font = font_file.to_vec();
+        Font::from_bytes(user_font).expect("Error constructing Font")
     };
 
-    // Init rendering
-    let mut plotter = Plotter::new(WIDTH as u32, HEIGHT as u32);
-    let mut raster = Raster::new(plotter.width(), plotter.height());
+    let fonts = vec![font];
+    let bounds = (WIDTH - (PADDING * 2.0), HEIGHT - (PADDING * 2.0));
 
-    // Render title left aligned with line wrapping
-    let mut begin = 0;
-    let mut line = 0;
-    loop {
-        let (path, l) = font.render(
-            &title[begin..],
-            (
-                PADDING,
-                line as f32 * title_font_size as f32 + PADDING,
-                WIDTH - (PADDING * 2.0),
-                HEIGHT,
-            ),
-            (title_font_size as f32, title_font_size as f32),
-            fonterator::TextAlign::Left,
-        );
-        let path: Vec<footile::PathOp> = path.collect();
-        raster.over(
-            plotter.fill(&path, FillRule::NonZero),
-            Rgba8::rgb(rgb_value.0, rgb_value.1, rgb_value.2),
-        );
-        begin += l;
-        line += 1;
-        if l == 0 {
-            break;
+    let title_glyphs: Vec<_> = Layout::default().calculate_glyphs(
+        &fonts,
+        &SectionGeometry {
+            screen_position: (PADDING, PADDING),
+            bounds: bounds,
+        },
+        &[SectionText {
+            text: title,
+            scale: Scale::uniform(title_font_size as f32),
+            font_id: FontId(0),
+            color: [0.0, 1.0, 0.0, 1.0],
+        }],
+    );
+
+    let author_glyphs: Vec<_> = Layout::default().calculate_glyphs(
+        &fonts,
+        &SectionGeometry {
+            screen_position: (PADDING, HEIGHT - PADDING - subtitle_font_size as f32),
+            bounds: bounds,
+        },
+        &[SectionText {
+            text: author,
+            scale: Scale::uniform(subtitle_font_size as f32),
+            font_id: FontId(0),
+            color: [0.0, 1.0, 0.0, 1.0],
+        }],
+    );
+
+    // Create a new rgba image
+    let mut image = DynamicImage::new_rgba8(WIDTH as u32, HEIGHT as u32).to_rgba();
+
+    for glyph in title_glyphs {
+        if let Some(bounding_box) = glyph.0.pixel_bounding_box() {
+            glyph.0.draw(|x, y, v| {
+                image.put_pixel(
+                    // Offset the position by the glyph bounding box
+                    x + bounding_box.min.x as u32,
+                    y + bounding_box.min.y as u32,
+                    Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
+                )
+            });
         }
     }
 
-    // Render author left aligned.
-    let path = font
-        .render(
-            author,
-            (
-                PADDING,
-                HEIGHT - subtitle_font_size as f32 - PADDING,
-                WIDTH - (PADDING * 2.0),
-                HEIGHT,
-            ),
-            (subtitle_font_size as f32, subtitle_font_size as f32),
-            fonterator::TextAlign::Left,
-        )
-        .0;
-    let path: Vec<fonterator::PathOp> = path.collect();
-    raster.over(
-        plotter.fill(&path, FillRule::NonZero),
-        Rgba8::rgb(rgb_value.0, rgb_value.1, rgb_value.2),
-    );
+    for glyph in author_glyphs {
+        if let Some(bounding_box) = glyph.0.pixel_bounding_box() {
+            glyph.0.draw(|x, y, v| {
+                image.put_pixel(
+                    // Offset the position by the glyph bounding box
+                    x + bounding_box.min.x as u32,
+                    y + bounding_box.min.y as u32,
+                    Rgba([rgb_value.0, rgb_value.1, rgb_value.2, (v * 255.0) as u8]),
+                )
+            });
+        }
+    }
 
-    // Return a Vec<T>
-    let x = raster.as_u8_slice().to_vec();
-    return x;
+    return image.to_vec();
 }
